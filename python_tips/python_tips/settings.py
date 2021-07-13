b9027f5cd5099b15b28da7b2b6ad9bc6782da644
Zues D'ghost <zuesghost1@gmail.com>
2021-07-13 10:09:30 +0100

    added updates

--- python_tips/python_tips/settings.py
+++ python_tips/python_tips/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #install apps
-    'auth',
+    'authentication',
     'tweets',
+    'social_auth',
 
     #3rd party installations
     'pillow',
