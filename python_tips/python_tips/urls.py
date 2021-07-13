9edf70e807584fc40abf1cc361106dbfb20a5cf2
Zues D'ghost <zuesghost1@gmail.com>
2021-07-13 10:34:03 +0100

    updated

--- python_tips/python_tips/urls.py
+++ python_tips/python_tips/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', include('auth.urls')),
+    path('', include('authentication.urls')),
     path('', include('tweets.urls')),
     path('home/', schema_view.with_ui('swagger',cache_timeout=0), name='schema-swagger-ui'),
     path('api/api.json/', schema_view.without_ui(cache_timeout=0), name='schema-swagger-ui'),
