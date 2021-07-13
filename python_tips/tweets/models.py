18fed704b3034219764bac9b1322cc65c6146f6d
Zues D'ghost <zuesghost1@gmail.com>
2021-07-13 10:32:57 +0100

    updated

--- python_tips/tweets/models.py
+++ python_tips/tweets/models.py
@@ -11,7 +11,7 @@ class Tweets(models.Model):
     tips = models.TextField(blank=False, null= False)
     save_to_database = models.DateTimeField(auto_now_add= True)
     timestamp = models.CharField(blank = False, null= False, max_length = 200)
-    link = models.UrlField(blank = True, null= True)
+    link = models.URLField(blank = True, null= True)
     likes = models.IntegerField(blank=True, null=True)
     retweets = models.IntegerField(blank=True, null=True)
     
